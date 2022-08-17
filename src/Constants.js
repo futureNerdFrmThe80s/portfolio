587e972778a58d072d5d58f358fbd19fcc525993
Greg Robertson <grobertson743@gmail.com>
2022-08-16 23:39:50 -0700

    Finsihed

--- src/Constants.js
+++ src/Constants.js
@@ -1,38 +1,30 @@
-import AUTOMATION from "./Assets/Projects/3automation.jpg";
-import COAST from "./Assets/Projects/coast.jpg";
-import COSMOS from "./Assets/Projects/cosmos.jpg";
-import TRAVELWELL from "./Assets/Projects/travelwell.png";
+import SCRUMPTIOUS from "./Assets/Projects/Scrumptious2.png";
+import BUCKET from "./Assets/Projects/DKTB.png";
+import CARCAR from "./Assets/Projects/CarCar.png";
 
 
 export const PROJECTS = [
   {
-    image: COSMOS,
+    image: BUCKET,
     name: "Don't Kick the Bucket",
-    technologyUsed: "Django,React, Hooks, Bootstrap, PostgreSQL, Heroku",
+    technologyUsed: "Django || React || Hooks || Bootstrap || PostgreSQL || Heroku || Docker",
     description: "Don't Kick the Bucket is a full-stack web app comprised of a 5 person dev team that leverages 3rd party APIs to create an extensive network of information for National Park/ Outdoor adventures",
     url: "https://gitlab.com/modzilla-mafia/dont-kick-the-bucket",
   },
   {
-    image: TRAVELWELL,
-    name: "travlwell.com",
-    technologyUsed: "Shopify, React, typescript, polaris, GraphQL",
-    description: "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
-    url: "https://travlwell.com/",
+    image: CARCAR,
+    name: "CarCar",
+    technologyUsed: "Django || React || Hooks || Bootstrap || PostgreSQL || Docker",
+    description: "Composed of a small 2 person dev team. CarCar is a full stack micro-service structured car dealership management app utilizing RESTful API that manages sales of vehicles, inventory, vehicle maintenance appointments and service technicians. ",
+    url: "https://gitlab.com/strhyne90/project-beta",
   },
   {
-    image: COAST,
-    name: "Coast",
-    technologyUsed: "ReactNative, Javascript, Redux",
-    description: "Team collaboration mobile app and online services like slack Message people and groups without sharing phone numbers, communicate with managers and staff all in one place. and discuss or assign tasks to get stuff done",
-    url: "https://play.google.com/store/apps/details?id=com.fomo.android.app",
-  },
-  {
-    image: AUTOMATION,
-    name: "3automation.com (RPA)",
-    technologyUsed: "ReactJs, Typescript, Redux",
-    description: "The next-gen Robotic Process Automation (#RPA) platform for retail and telecom domains. The ultimate automation platform for your Shopify, Email, E- commerce shop",
-    url: "https://www.3automation.com/",
-  },
+    image: SCRUMPTIOUS,
+    name: "Scrumptious",
+    technologyUsed: "Django || SQLite || CSS || HTML",
+    description: "A solo project, full stack web application that tracks and stores recipes conveniently. Add recipes to your cookbook, dynamically render measurements, comment and rate other recipes ",
+    url: "https://gitlab.com/grobertson743/scrumptious-recipes",
+  }
 
 ];
 
